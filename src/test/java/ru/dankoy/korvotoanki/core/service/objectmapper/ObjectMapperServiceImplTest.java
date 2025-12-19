a1b7ca21144ec8dfc7b099212fed6923263a5957
Evgeny <transingularity@gmail.com>
2025-12-19 08:25:06 +0300

    style: style

--- src/test/java/ru/dankoy/korvotoanki/core/service/objectmapper/ObjectMapperServiceImplTest.java
+++ src/test/java/ru/dankoy/korvotoanki/core/service/objectmapper/ObjectMapperServiceImplTest.java
@@ -14,7 +14,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
 import ru.dankoy.korvotoanki.core.exceptions.ObjectMapperException;
 
 @DisplayName("Test ObjectMapperService ")
